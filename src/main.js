5526514b278818e35210d8bc31c2f15de70fbbf5
loiufam <luo_yaohui88@163.com>
2025-02-19 14:00:07 +0800

    图片懒加载指令实现

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { useIntersectionObserver } from '@vueuse/core'
 
 import App from './App.vue'
 import router from './router'
@@ -15,3 +16,20 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// 定义全局指令
+
+app.directive('img-lazy', {
+  mounted(el, binding) {
+    // console.log(el, binding.value)
+  useIntersectionObserver(
+    el,
+    ([{isIntersecting}]) => {
+      // 进入可视区域
+      if (isIntersecting) {
+        el.src = binding.value
+      }
+    },
+  )
+  }
+})
