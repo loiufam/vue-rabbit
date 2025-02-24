2e7ba8b4d411e4cccbe5fef03c1f8b6f1c9ee8c2
loiufam <luo_yaohui88@163.com>
2025-02-24 15:18:36 +0800

    结算页

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Details/index.vue'
+import CartList from '@/views/CartList/index.vue'
+import CheckOut from '@/views/CheckOut/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +30,14 @@ const router = createRouter({
         {
           path: 'detail/:id',
           component: Detail
+        },
+        {
+          path: 'cartlist',
+          component: CartList
+        },
+        {
+          path: 'checkout',
+          component: CheckOut
         }
       ],
     },
